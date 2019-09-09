dbc36cf2d87dc231c24e2717f642143804d3b346
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-09-09 09:50:50 +0900

    [change] Unit Testの記述方法を公式ドキュメントに記載しているやり方に変更

--- components/molecules/button/__tests__/AddButton.spec.ts
+++ components/molecules/button/__tests__/AddButton.spec.ts
@@ -1,26 +1,48 @@
-import { mount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+import { mount, createLocalVue } from '@vue/test-utils'
 import AddButton from '@/components/molecules/button/AddButton.vue'
 
+const localVue = createLocalVue()
+
 describe('molecules/button/AddButton.vue', () => {
+  let vuetify: typeof Vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   it('snapshot', () => {
-    const wrapper = mount(AddButton)
+    const wrapper = mount(AddButton, {
+      localVue,
+      vuetify
+    })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('colorを指定しない場合、デフォルトの色が指定される', () => {
-    const wrapper = mount(AddButton)
+    const wrapper = mount(AddButton, {
+      localVue,
+      vuetify
+    })
     expect(wrapper.find('button').classes()).toContain('teal')
   })
 
   it('colorに色を指定した場合、その指定した色が指定される', () => {
     const color = 'red'
-    const wrapper = mount(AddButton, { propsData: { color } })
+    const wrapper = mount(AddButton, {
+      localVue,
+      vuetify,
+      propsData: { color }
+    })
     expect(wrapper.find('button').classes()).toContain('red')
   })
 
   it('clickした場合、@clickで登録した関数が実行される', () => {
     const click = jest.fn()
-    const wrapper = mount(AddButton)
+    const wrapper = mount(AddButton, {
+      localVue,
+      vuetify
+    })
     wrapper.vm.$on('click', click)
     wrapper.trigger('click')
 
--- components/molecules/button/__tests__/NavigationButton.spec.ts
+++ components/molecules/button/__tests__/NavigationButton.spec.ts
@@ -1,9 +1,20 @@
-import { mount } from '@vue/test-utils'
+import Vuetify from 'vuetify'
+import { mount, createLocalVue } from '@vue/test-utils'
 import NavigationButton from '@/components/molecules/button/NavigationButton.vue'
 
+const localVue = createLocalVue()
+
 describe('molecules/button/NavigationButton.vue', () => {
+  let vuetify: typeof Vuetify
+
+  beforeEach(() => {
+    vuetify = new Vuetify()
+  })
+
   it('snapshot', () => {
     const wrapper = mount(NavigationButton, {
+      localVue,
+      vuetify,
       propsData: {
         value: 'workouts',
         text: 'Workouts',
