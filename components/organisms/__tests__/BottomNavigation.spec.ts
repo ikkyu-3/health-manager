dc8c2b285271d08d31d75ef4d02c5f5f5837c254
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-09-30 09:02:53 +0900

    [change] BottomNavigationコンポーネントのテストケースを追加

--- components/organisms/__tests__/BottomNavigation.spec.ts
+++ components/organisms/__tests__/BottomNavigation.spec.ts
@@ -1,18 +1,69 @@
+import Vuex from 'vuex'
 import Vuetify from 'vuetify'
 import { mount, createLocalVue, RouterLinkStub } from '@vue/test-utils'
 import BottomNavigation from '@/components/organisms/BottomNavigation.vue'
 
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
 const options = {
-  localVue: createLocalVue(),
+  localVue,
   vuetify: new Vuetify(),
   stubs: {
     NuxtLink: RouterLinkStub
   }
 }
 
+const actions = { changeTitle: jest.fn() }
+jest.mock('@/store', () => ({
+  userStore: () => new Vuex.Store({ state: {}, actions })
+}))
+
 describe('organisms/BottomNavigation.vue', () => {
   it('snapshot', () => {
     const wrapper = mount(BottomNavigation, { ...options })
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  describe('button click', () => {
+    beforeEach(() => {
+      actions.changeTitle.mockClear()
+    })
+
+    it('"Workouts"をクリックすると、"changeTitle"アクションが呼ばれる', () => {
+      const wrapper = mount(BottomNavigation, { ...options })
+      const button = wrapper.find('a[value="workouts"]')
+      button.trigger('click')
+      expect(actions.changeTitle.mock.calls[0][1]).toEqual({
+        title: 'Workouts'
+      })
+    })
+
+    it('"Schedule"をクリックすると、"changeTitle"アクションが呼ばれる', () => {
+      const wrapper = mount(BottomNavigation, { ...options })
+      const button = wrapper.find('a[value="schedule"]')
+      button.trigger('click')
+      expect(actions.changeTitle.mock.calls[0][1]).toEqual({
+        title: 'Schedule'
+      })
+    })
+
+    it('"Workouts Data"をクリックすると、"changeTitle"アクションが呼ばれる', () => {
+      const wrapper = mount(BottomNavigation, { ...options })
+      const button = wrapper.find('a[value="workouts-data"]')
+      button.trigger('click')
+      expect(actions.changeTitle.mock.calls[0][1]).toEqual({
+        title: 'Workouts Data'
+      })
+    })
+
+    it('"Health Planet"をクリックすると、"changeTitle"アクションが呼ばれる', () => {
+      const wrapper = mount(BottomNavigation, { ...options })
+      const button = wrapper.find('a[value="health-planet"]')
+      button.trigger('click')
+      expect(actions.changeTitle.mock.calls[0][1]).toEqual({
+        title: 'Health Planet'
+      })
+    })
   })
 })
