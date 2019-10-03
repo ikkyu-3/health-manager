3c0fc295a906b9f92f14a0f154c402132dc1b13d
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-03 23:13:58 +0900

    [change] organismsのstorybookを更新

--- components/organisms/organism.stories.ts
+++ components/organisms/organism.stories.ts
@@ -1,5 +1,15 @@
 import { storiesOf } from '@storybook/vue'
+import Vuex from 'vuex'
 import BottomNavigation from '@/components/organisms/BottomNavigation.vue'
+import Toolbar from '@/components/organisms/Toolbar.vue'
+
+storiesOf('Organisms', module).add('Toolbar', () => ({
+  store: new Vuex.Store({
+    state: { currentPage: 'Workouts' }
+  }),
+  components: { Toolbar },
+  template: '<toolbar />'
+}))
 
 storiesOf('Organisms', module).add('BottomNavigation', () => ({
   components: { BottomNavigation },
