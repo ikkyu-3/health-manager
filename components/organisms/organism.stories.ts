befadbdea886442cc0869b170052101249312d07
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-06 17:27:18 +0900

    [change] storybookにDialogToolbarを追加

--- components/organisms/organism.stories.ts
+++ components/organisms/organism.stories.ts
@@ -1,7 +1,9 @@
 import { storiesOf } from '@storybook/vue'
 import Vuex from 'vuex'
-import BottomNavigation from '@/components/organisms/BottomNavigation.vue'
+import { action } from '@storybook/addon-actions'
 import Toolbar from '@/components/organisms/Toolbar.vue'
+import DialogToolbar from '@/components/organisms/DialogToolbar.vue'
+import BottomNavigation from '@/components/organisms/BottomNavigation.vue'
 
 storiesOf('Organisms', module).add('Toolbar', () => ({
   store: new Vuex.Store({
@@ -9,6 +11,18 @@ storiesOf('Organisms', module).add('Toolbar', () => ({
   }),
   components: { Toolbar },
   template: '<toolbar />'
+}))
+
+storiesOf('Organisms', module).add('DialogToolbar', () => ({
+  components: { DialogToolbar },
+  template:
+    '<dialog-toolbar :arrowButtonClick="arrowButtonClick" :clearButtonClick="clearButtonClick"/>',
+  data: () => {
+    return {
+      arrowButtonClick: action('Arrow button clicked'),
+      clearButtonClick: action('Clear button clicked')
+    }
+  }
 }))
 
 storiesOf('Organisms', module).add('BottomNavigation', () => ({
