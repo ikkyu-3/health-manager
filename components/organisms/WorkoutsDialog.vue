bb871b1a27851b6731c0cb999538d46d636361f3
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-07 23:43:40 +0900

    [add/change] organisms/WorkoutsDialogを追加

---
+++ components/organisms/WorkoutsDialog.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    fullscreen
+    hide-overlay
+    transition="dialog-bottom-transition"
+  >
+    <v-card>
+      <dialog-toolbar :arrow-button-click="back" :clear-button-click="clear" />
+      <v-list three-line subheader>
+        <v-subheader>Training Menu</v-subheader>
+      </v-list>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+import DialogToolbar from '@/components/organisms/DialogToolbar.vue'
+import WorkoutListItem from '@/components/molecules/listItem/WorkoutListItem.vue'
+
+export default createComponent({
+  props: {
+    dialog: {
+      type: Boolean,
+      require: true
+    },
+    back: {
+      type: Function,
+      require: true
+    },
+    clear: {
+      type: Function,
+      require: true
+    }
+  },
+  components: { DialogToolbar, WorkoutListItem }
+})
+</script>
--- components/organisms/organism.stories.ts
+++ components/organisms/organism.stories.ts
@@ -4,6 +4,7 @@ import { action } from '@storybook/addon-actions'
 import Toolbar from '@/components/organisms/Toolbar.vue'
 import DialogToolbar from '@/components/organisms/DialogToolbar.vue'
 import BottomNavigation from '@/components/organisms/BottomNavigation.vue'
+import WorkoutsDialog from '@/components/organisms/WorkoutsDialog.vue'
 
 storiesOf('Organisms', module).add('Toolbar', () => ({
   store: new Vuex.Store({
@@ -17,15 +18,22 @@ storiesOf('Organisms', module).add('DialogToolbar', () => ({
   components: { DialogToolbar },
   template:
     '<dialog-toolbar :arrowButtonClick="arrowButtonClick" :clearButtonClick="clearButtonClick"/>',
-  data: () => {
-    return {
-      arrowButtonClick: action('Arrow button clicked'),
-      clearButtonClick: action('Clear button clicked')
-    }
+  methods: {
+    arrowButtonClick: action('Arrow button clicked'),
+    clearButtonClick: action('Clear button clicked')
   }
 }))
 
 storiesOf('Organisms', module).add('BottomNavigation', () => ({
   components: { BottomNavigation },
   template: '<bottom-navigation />'
+}))
+
+storiesOf('Organisms', module).add('WorkoutsDialog', () => ({
+  components: { WorkoutsDialog },
+  template: '<workouts-dialog :dialog="true" :clear="clear" :back="back"/>',
+  methods: {
+    clear: action('Clear List'),
+    back: action('Back Page')
+  }
 }))
