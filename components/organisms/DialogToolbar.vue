a0de4a24ec081fb0f31e073c90dfcd10d3ec7c2d
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-24 01:04:28 +0900

    [change] v-app-barを使用するため変更

--- components/organisms/DialogToolbar.vue
+++ components/organisms/DialogToolbar.vue
@@ -1,14 +1,12 @@
 <template>
-  <v-toolbar dark :color="state.color">
+  <v-app-bar dark fixed :color="state.color">
     <v-btn icon @click="arrowButtonClick">
       <v-icon>fa-arrow-left</v-icon>
     </v-btn>
     <v-toolbar-title>Workoutsを追加</v-toolbar-title>
-    <div class="flex-grow-1"></div>
-    <v-toolbar-items>
-      <v-btn text @click="clearButtonClick">Clear</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+    <v-spacer></v-spacer>
+    <v-btn text @click="clearButtonClick">Clear</v-btn>
+  </v-app-bar>
 </template>
 
 <script lang="ts">
