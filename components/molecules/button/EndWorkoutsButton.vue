6b314798a3d5c48c9bd9fd050dd73246af4daaf9
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-25 01:58:30 +0900

    [add] molecules/button/EndWorkoutsButton追加

---
+++ components/molecules/button/EndWorkoutsButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn class="end-workouts-button" color="#ef9a9a" dark @click="click"
+    >End Workouts</v-btn
+  >
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  setup(_, { emit }) {
+    function click(e: MouseEvent) {
+      emit('click', e)
+    }
+
+    return { click }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.end-workouts-button {
+  width: 100%;
+}
+</style>
---
+++ components/molecules/button/__tests__/EndWorkoutsButton.spec.ts
@@ -0,0 +1,20 @@
+import Vuetify from 'vuetify'
+import { mount, createLocalVue } from '@vue/test-utils'
+import EndWorkoutsButton from '@/components/molecules/button/EndWorkoutsButton.vue'
+
+const options = {
+  localVue: createLocalVue(),
+  vuetify: new Vuetify()
+}
+
+describe('molecules/button/EndWorkoutsButton.vue', () => {
+  it('clickした場合、@clickに渡した関数が実行される', () => {
+    const click = jest.fn()
+    const wrapper = mount(EndWorkoutsButton, { ...options })
+
+    wrapper.vm.$on('click', click)
+    wrapper.trigger('click')
+
+    expect(click.mock.calls).toHaveLength(1)
+  })
+})
