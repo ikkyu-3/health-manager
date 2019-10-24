9be66a3a5ee5e4f2b93a4526eb02c8a15c3f7744
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-25 01:59:17 +0900

    [change] storybookにEndWorkoutsButtonを追加

--- components/molecules/button/button.stories.ts
+++ components/molecules/button/button.stories.ts
@@ -1,9 +1,19 @@
 import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 import AddButton from '@/components/molecules/button/AddButton.vue'
+import EndWorkoutsButton from '@/components/molecules/button/EndWorkoutsButton.vue'
 
 storiesOf('Molecules.Button.AddButton', module).add('default color', () => ({
   components: { AddButton },
   template: '<add-button @click="action"/>',
   methods: { action: action('clicked') }
 }))
+
+storiesOf('Molecules.Button.EndWorkoutsButton', module).add(
+  'default color',
+  () => ({
+    components: { EndWorkoutsButton },
+    template: '<end-workouts-button @click="action"/>',
+    methods: { action: action('clicked') }
+  })
+)
