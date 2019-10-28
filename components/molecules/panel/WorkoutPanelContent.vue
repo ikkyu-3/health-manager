4599b7a2488878fb855f5512d91489955536c393
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-29 00:43:51 +0900

    [add] molecules/panel/WorkoutPanelContent追加

---
+++ components/molecules/panel/WorkoutPanelContent.vue
@@ -0,0 +1,27 @@
+<template>
+  <component :is="state.componentName" :workout="workout" />
+</template>
+
+<script lang="ts">
+import { createComponent, reactive } from '@vue/composition-api'
+import LegPressContent from './Content/LegPressContent.vue'
+import { Workout } from '@/types'
+
+type WorkoutPanelContentProps = {
+  workout: Workout
+}
+
+export default createComponent<WorkoutPanelContentProps, {}>({
+  components: { LegPressContent },
+  props: {
+    workout: Object
+  },
+  setup(props) {
+    const state = reactive({
+      componentName: `${props.workout.name.replace(/\s+/g, '')}Content`
+    })
+
+    return { state }
+  }
+})
+</script>
