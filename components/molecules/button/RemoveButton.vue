553ebab51f107b0b9713878ab2fcf446659db182
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-10-30 23:03:33 +0900

    [add] molecules/button/RemoveButtonを追加

---
+++ components/molecules/button/RemoveButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-btn fab dark :color="color" :class="className" @click="click">
+    <v-icon>fa-minus</v-icon>
+  </v-btn>
+</template>
+
+<script lang="ts">
+import { createComponent } from '@vue/composition-api'
+
+export default createComponent({
+  props: {
+    color: {
+      type: String,
+      default: 'grey'
+    }
+  },
+  setup(_, { emit }) {
+    function click(e: MouseEvent) {
+      emit('click', e)
+    }
+
+    return { click }
+  }
+})
+</script>
--- components/molecules/button/button.stories.ts
+++ components/molecules/button/button.stories.ts
@@ -1,11 +1,18 @@
 import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 import AddButton from '@/components/molecules/button/AddButton.vue'
+import RemoveButton from '@/components/molecules/button/RemoveButton.vue'
 import EndWorkoutsButton from '@/components/molecules/button/EndWorkoutsButton.vue'
 
 storiesOf('Molecules.Button.AddButton', module).add('default color', () => ({
   components: { AddButton },
   template: '<add-button @click="action"/>',
+  methods: { action: action('clicked') }
+}))
+
+storiesOf('Molecules.Button.RemoveButton', module).add('default color', () => ({
+  components: { RemoveButton },
+  template: '<remove-button @click="action"/>',
   methods: { action: action('clicked') }
 }))
 
