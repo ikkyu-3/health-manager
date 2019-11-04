6615bec69246e25b75fdb1118c90ede2e9ca037e
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-11-04 20:32:15 +0900

    [change] storybook-addon-vue-infoを導入

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,15 +2,13 @@ import '@fortawesome/fontawesome-free/css/all.css'
 import 'vuetify/dist/vuetify.min.css'
 import { configure, addDecorator } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
+import { withInfo, setDefaults } from 'storybook-addon-vue-info'
+
 import Vue from 'vue'
-import Vuex from 'vuex'
-import VueRouter from 'vue-router'
 import Vuetify from 'vuetify'
 import VueCompositionApi from "@vue/composition-api";
 
 Vue.use(VueCompositionApi);
-Vue.use(Vuex)
-Vue.use(VueRouter)
 Vue.use(Vuetify)
 
 Vue.component('nuxt-link', {
@@ -23,35 +21,20 @@ Vue.component('nuxt-link', {
   template: '<a href="#" @click.prevent="log()"><slot>NuxtLink</slot></a>'
 })
 
-const store = new Vuex.Store({
-  state: () => ({ currentPage: 'Hoge' })
-})
-
-const router = new VueRouter({ routes: [{ path: '/'}] })
-
 const vuetifyConfig = new Vuetify({
-  icons: {
-    iconfont: 'fa'
-  },
-  theme: {
-    dark: false
-  }
+  icons: { iconfont: 'fa' },
+  theme: { dark: false }
 })
 
 addDecorator(() => {
   return {
-    router,
-    store,
     vuetify: vuetifyConfig,
-    created: function() {
-      this.$root.$store = this.$store;
-      this.$root._route = this.$route;
-      this.$root._router = router;
-      this.$root.$vuetify = this.$vuetify;
-    },
     template: '<v-app><div><story/></div></v-app>'
   }
 })
+
+addDecorator(withInfo)
+setDefaults({ header: false })
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../', true, /\.stories\.ts$/)
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -44,6 +44,12 @@ module.exports = ({ config }) => {
     ]
   })
 
+  config.module.rules.push({
+    test: /\.vue$/,
+    loader: 'storybook-addon-vue-info/loader',
+    enforce: 'post'
+  })
+
   config.plugins.push(new ForkTsCheckerWebpackPlugin())
   config.plugins.push(new StylelintPlugin({
     files: [
