1229a27311eaf99c68a731b80b0a11af9d59d77f
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-11-17 14:16:21 +0900

    [change] store分割 index.ts, type.tsを変更

--- store/index.ts
+++ store/index.ts
@@ -1,28 +1,17 @@
 import Vue from 'vue'
-import Vuex, { ActionContext } from 'vuex'
+import Vuex from 'vuex'
 import workouts from './workouts'
+import mutations from './mutations'
+import actions from './actions'
+import { RootState } from './type'
 import { bottomNavigation } from '@/constants'
 
 Vue.use(Vuex)
 
-export type RootState = {
-  title: string
-}
-
 const store = new Vuex.Store<RootState>({
-  state: {
-    title: bottomNavigation[0].text
-  },
-  mutations: {
-    changeTitle(state: RootState, payload: any) {
-      state.title = payload.title
-    }
-  },
-  actions: {
-    changeTitle(context: ActionContext<RootState, any>, payload: { title: 1 }) {
-      context.commit('changeTitle', { title: payload.title })
-    }
-  },
+  state: { title: bottomNavigation[0].text },
+  mutations,
+  actions,
   modules: { workouts }
 })
 
--- store/type.ts
+++ store/type.ts
@@ -1,1 +1,7 @@
 export type RootState = { title: string }
+
+// actions
+export type changeTitleActionPayload = { title: string }
+
+// mutations
+export type changeTitleMutationPayload = { title: string }
