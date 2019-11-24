8c5b76e871f5f375b1bd6c6328f5e844e2bd6ed9
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-11-24 18:17:38 +0900

    [add] modules/getPeriod追加

---
+++ modules/getPeriod.ts
@@ -0,0 +1,27 @@
+function isISOString(str: string) {
+  return /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/.test(str)
+}
+
+export default function getPeriod(startTime: string, endTime: string) {
+  if (!isISOString(startTime)) {
+    console.warn('startTime is invalid', startTime) // eslint-disable-line no-console
+    return '--:--:--'
+  }
+
+  if (!isISOString(endTime)) {
+    console.warn('endTime is invalid', endTime) // eslint-disable-line no-console
+    return '--:--:--'
+  }
+
+  const start = Date.parse(startTime)
+  const end = Date.parse(endTime)
+
+  const diffSecondTime = Math.floor((end - start) / 1000)
+  const hour = Math.floor(diffSecondTime / 60 ** 2)
+  const minute = Math.floor(diffSecondTime / 60) - hour * 60
+  const second = diffSecondTime % 60
+
+  return `${hour.toString().padStart(2, '0')}:${minute
+    .toString()
+    .padStart(2, '0')}:${second.toString().padStart(2, '0')}`
+}
