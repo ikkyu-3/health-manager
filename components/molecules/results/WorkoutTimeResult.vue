e489dc0877a96de50c25f5654d894705c8aeb559
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-11-30 15:29:33 +0900

    [add] molecules/results/WorkoutTimeResult.vue

---
+++ components/molecules/results/WorkoutTimeResult.vue
@@ -0,0 +1,40 @@
+<template>
+  <dl>
+    <dt class="title">Workout Time</dt>
+    <dd class="data">{{ state.workoutTime }}</dd>
+  </dl>
+</template>
+
+<script lang="ts">
+import { createComponent, reactive } from '@vue/composition-api'
+import getPeriod from '@/modules/getPeriod'
+
+export type WorkoutTimeProps = {
+  startTime: string
+  endTime: string
+}
+
+export default createComponent<WorkoutTimeProps, {}>({
+  props: {
+    startTime: String,
+    endTime: String
+  },
+  setup({ startTime, endTime }) {
+    const state = reactive({
+      workoutTime: getPeriod(startTime, endTime)
+    })
+
+    return { state }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.title {
+  margin: 8px 0 4px;
+}
+
+.data {
+  margin-left: 12px;
+}
+</style>
