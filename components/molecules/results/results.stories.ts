079a0ac673f3f6b18c535ffb6e5360684d33e93a
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-12-09 23:39:01 +0900

    [add/change] molecules/results/LegCurlResult追加

--- components/molecules/results/results.stories.ts
+++ components/molecules/results/results.stories.ts
@@ -1,25 +1,19 @@
 import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 import BaseResult from './BaseResult.vue'
-import LegPressResult, { initResult } from './LegPressResult.vue'
 import WorkoutTimeResult from './WorkoutTimeResult.vue'
+import LegPressResult, {
+  initResult as legPressInitResult
+} from './LegPressResult.vue'
+import LegCurlResult, {
+  initResult as legCurlInitResult
+} from './LegCurlResult.vue'
 
 storiesOf('molecules.results', module).add('BaseResult', () => ({
   components: { BaseResult },
   template: `
     <base-result :delete-result="action" style="height: 200px;" >Base Result</base-result>
   `,
-  methods: {
-    action: action('clicked')
-  }
-}))
-
-storiesOf('molecules.results', module).add('LegPressResult', () => ({
-  components: { LegPressResult },
-  template: `
-    <leg-press-result :index="index" :result="result" :delete-result="action"/>
-  `,
-  data: () => ({ index: 0, result: initResult() }),
   methods: {
     action: action('clicked')
   }
@@ -34,4 +28,29 @@ storiesOf('molecules.results', module).add('WorkoutTimeResult', () => ({
     startTime: '2019-01-01T00:00:00.000Z',
     endTime: '2019-01-01T00:01:30.000Z'
   })
+}))
+
+storiesOf('molecules.results', module).add('WorkoutResult', () => ({
+  components: { LegPressResult, LegCurlResult },
+  template: `
+    <article>
+      <h1>WorkoutResult</h1>
+      <section>
+        <h2>LegPressResult</h2>
+        <leg-press-result :index="index" :result="legPressResult" :delete-result="action"/>
+      </section>
+      <section>
+        <h2>LegPressResult</h2>
+        <leg-curl-result :index="index" :result="legCurlResult" :delete-result="action"/>
+      </section>
+    </article>
+  `,
+  data: () => ({
+    index: 0,
+    legPressResult: legPressInitResult(),
+    legCurlResult: legCurlInitResult()
+  }),
+  methods: {
+    action: action('clicked')
+  }
 }))
