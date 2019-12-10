1b1bf5fdb05be8e355557dddc7491ea588e7a3a3
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-12-11 01:24:03 +0900

    [add/change] molecules/resultsの種類を増やした

---
+++ components/molecules/results/SitUpResult.vue
@@ -0,0 +1,106 @@
+<template>
+  <base-result :delete-result="() => deleteResult(index)">
+    <v-row class="row">
+      <v-col cols="5" class="key">Times</v-col>
+      <v-col cols="7">
+        <result-field
+          times
+          :value="state.times"
+          :reduce="reduceTimes"
+          :add="addTimes"
+        />
+      </v-col>
+      <v-col cols="5" class="key">Set</v-col>
+      <v-col cols="7">
+        <result-field
+          set
+          :value="state.set"
+          :reduce="reduceSet"
+          :add="addSet"
+        />
+      </v-col>
+    </v-row>
+  </base-result>
+</template>
+
+<script lang="ts">
+import { createComponent, reactive } from '@vue/composition-api'
+import { WeightTrainingResult } from '@/types'
+import BaseResult from '@/components/molecules/results/BaseResult.vue'
+import ResultField from '@/components/molecules/fields/ResultField.vue'
+
+const config = {
+  times: { max: 50, default: 10, step: 1 },
+  set: { max: 10, default: 3, step: 1 }
+}
+
+type SitUpResultType = {
+  index: number
+  result: {
+    times: number
+    set: number
+  }
+  deleteResult: (index: number) => void
+}
+
+export const initResult = (): WeightTrainingResult => ({
+  times: config.times.default,
+  set: config.set.default
+})
+
+export default createComponent<SitUpResultType, {}>({
+  components: { BaseResult, ResultField },
+  props: {
+    index: Number,
+    result: Object,
+    deleteResult: Function
+  },
+  setup(props, { emit }) {
+    const { index, result } = props
+    const { times, set } = result
+    const state = reactive({ times, set })
+
+    const reduceTimes = (e: MouseEvent) => {
+      e.stopPropagation()
+      if (state.times === 0) return
+      state.times -= config.times.step
+      emit('times-change', index, state.times)
+    }
+
+    const addTimes = (e: MouseEvent) => {
+      e.stopPropagation()
+      if (state.times === config.times.max) return
+      state.times += config.times.step
+      emit('times-change', index, state.times)
+    }
+
+    const reduceSet = (e: MouseEvent) => {
+      e.stopPropagation()
+      if (state.set === 0) return
+      state.set -= config.set.step
+      emit('set-change', index, state.times)
+    }
+
+    const addSet = (e: MouseEvent) => {
+      e.stopPropagation()
+      if (state.set === config.set.max) return
+      state.set += config.set.step
+      emit('set-change', index, state.times)
+    }
+
+    return {
+      state,
+      reduceTimes,
+      addTimes,
+      reduceSet,
+      addSet
+    }
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.row {
+  margin: 0;
+}
+</style>
--- components/molecules/results/results.stories.ts
+++ components/molecules/results/results.stories.ts
@@ -2,6 +2,20 @@ import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
 import BaseResult from './BaseResult.vue'
 import WorkoutTimeResult from './WorkoutTimeResult.vue'
+
+// workout result
+import BenchPressResult, {
+  initResult as benchPressInitResult
+} from './BenchPressResult.vue'
+import ChestPressResult, {
+  initResult as chestPressInitResult
+} from './ChestPressResult.vue'
+import CrunchResult, {
+  initResult as crunchInitResult
+} from './CrunchResult.vue'
+import LatPulldownResult, {
+  initResult as latPulldownInitResult
+} from './LatPulldownResult.vue'
 import LegPressResult, {
   initResult as legPressInitResult
 } from './LegPressResult.vue'
@@ -11,6 +25,10 @@ import LegCurlResult, {
 import LegExtensionResult, {
   initResult as legExtensionInitResult
 } from './LegExtensionResult.vue'
+import LegRaiseResult, {
+  initResult as legRaiseInitResult
+} from './LegRaiseResult.vue'
+import SitUpResult, { initResult as sitUpInitResult } from './SitUpResult.vue'
 
 storiesOf('molecules.results', module).add('BaseResult', () => ({
   components: { BaseResult },
@@ -34,29 +52,69 @@ storiesOf('molecules.results', module).add('WorkoutTimeResult', () => ({
 }))
 
 storiesOf('molecules.results', module).add('WorkoutResult', () => ({
-  components: { LegPressResult, LegCurlResult, LegExtensionResult },
+  components: {
+    BenchPressResult,
+    ChestPressResult,
+    CrunchResult,
+    LatPulldownResult,
+    LegPressResult,
+    LegCurlResult,
+    LegExtensionResult,
+    LegRaiseResult,
+    SitUpResult
+  },
   template: `
     <article>
       <h1>WorkoutResult</h1>
+      <section>
+        <h2>BenchPressResult</h2>
+        <bench-press-result :index="index" :result="benchPressResult" :delete-result="action"/>
+      </section>
+      <section>
+        <h2>ChestPressResult</h2>
+        <chest-press-result :index="index" :result="chestPressResult" :delete-result="action"/>
+      </section>
+      <section>
+        <h2>CrunchResult</h2>
+        <crunch-result :index="index" :result="crunchResult" :delete-result="action"/>
+      </section>
+      <section>
+        <h2>LatPulldownResult</h2>
+        <lat-pulldown-result :index="index" :result="latPulldownResult" :delete-result="action"/>
+      </section>
       <section>
         <h2>LegPressResult</h2>
         <leg-press-result :index="index" :result="legPressResult" :delete-result="action"/>
       </section>
       <section>
-        <h2>LegPressResult</h2>
+        <h2>LegCurlResult</h2>
         <leg-curl-result :index="index" :result="legCurlResult" :delete-result="action"/>
       </section>
       <section>
         <h2>LegExtensionResult</h2>
         <leg-extension-result :index="index" :result="legExtensionResult" :delete-result="action"/>
       </section>
+      <section>
+        <h2>LegRaiseResult</h2>
+        <leg-raise-result :index="index" :result="legRaiseResult" :delete-result="action"/>
+      </section>
+      <section>
+        <h2>SitUpResult</h2>
+        <sit-up-result :index="index" :result="sitUpResult" :delete-result="action"/>
+      </section>
     </article>
   `,
   data: () => ({
     index: 0,
+    benchPressResult: benchPressInitResult(),
+    chestPressResult: chestPressInitResult(),
+    crunchResult: crunchInitResult(),
+    latPulldownResult: latPulldownInitResult(),
     legPressResult: legPressInitResult(),
     legCurlResult: legCurlInitResult(),
-    legExtensionResult: legExtensionInitResult()
+    legExtensionResult: legExtensionInitResult(),
+    legRaiseResult: legRaiseInitResult(),
+    sitUpResult: sitUpInitResult()
   }),
   methods: {
     action: action('clicked')
