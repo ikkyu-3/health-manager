11aa19210dbfc2bc3d66374e68d3db8b3645be2b
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2019-12-22 22:26:33 +0900

    [change] workoutsページに保存機能を追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,41 +1,95 @@
 <template>
   <v-layout :class="{ 'fill-height': !state.workoutsExists }">
-    <workout-panels v-if="state.workoutsExists" />
-    <p v-else class="empty">Let's Start Workouts!!💪</p>
+    <workout-panels v-if="state.workoutsExists" :save="save" />
+    <p v-else class="empty">{{ emptyText }}</p>
     <v-btn class="add" fixed fab dark color="lime" @click="showDialog">
       <v-icon>fa-plus</v-icon>
     </v-btn>
     <workouts-dialog :dialog="state.dialog" :back="hideDialog" />
+    <v-snackbar v-model="state.snackbar">
+      {{ state.snackbarText }}
+      <v-btn color="pink" text @click="hideSnackbar">
+        Close
+      </v-btn>
+    </v-snackbar>
   </v-layout>
 </template>
 
 <script lang="ts">
-import { createComponent, reactive, computed } from '@vue/composition-api'
+import {
+  createComponent,
+  reactive,
+  computed,
+  onMounted
+} from '@vue/composition-api'
 import WorkoutsDialog from '@/components/organisms/dialog/WorkoutsDialog.vue'
 import WorkoutPanels from '@/components/organisms/panels/WorkoutPanels.vue'
 import { subColor } from '@/constants'
 import { userStore } from '@/store'
+import IndexedDB from '@/modules/IndexedDB'
 
 export default createComponent({
   components: { WorkoutsDialog, WorkoutPanels },
   setup() {
     const store = userStore()
+    const db = new IndexedDB()
 
     const state = reactive({
       dialog: false,
       color: subColor,
-      workoutsExists: computed(() => store.getters['workouts/workoutsExists'])
+      workoutsExists: computed(() => store.getters['workouts/workoutsExists']),
+      canUseDb: false,
+      snackbarText: '',
+      snackbar: false
     })
 
-    function showDialog() {
+    const emptyText = `ワークアウトをはじめましょう!!💪`
+
+    const showDialog = () => {
       state.dialog = true
     }
 
-    function hideDialog() {
+    const hideDialog = () => {
       state.dialog = false
     }
 
-    return { state, showDialog, hideDialog }
+    const hideSnackbar = () => {
+      state.snackbar = false
+      state.snackbarText = ''
+    }
+
+    const save = () => {
+      if (state.canUseDb) {
+        db.save(store.getters['workouts/workouts'])
+          .then(() => {
+            state.snackbarText = `保存しました💪`
+            state.snackbar = true
+          })
+          .catch(() => {
+            state.snackbarText = `保存に失敗しました...😇`
+            state.snackbar = true
+          })
+      }
+    }
+
+    onMounted(() =>
+      db
+        .init()
+        .then(() => (state.canUseDb = true))
+        .catch(() => {
+          state.snackbarText = `保存機能が使用できません...😇`
+          state.snackbar = true
+        })
+    )
+
+    return {
+      state,
+      emptyText,
+      showDialog,
+      hideDialog,
+      hideSnackbar,
+      save
+    }
   }
 })
 </script>
