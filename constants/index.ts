8365efa95bf0f47b4c7fa97b5360cf6a265bae3f
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2020-01-18 16:17:43 +0900

    [fix] トレーニングメニューが重複していたバグを修正

--- constants/index.ts
+++ constants/index.ts
@@ -44,7 +44,7 @@ export const workoutMenu = [
   },
   {
     target: 'Back',
-    workouts: ['Lat Pulldown', 'Back Extension', 'Back Extension']
+    workouts: ['Lat Pulldown', 'Back Extension']
   },
   {
     target: 'Pectoral',
